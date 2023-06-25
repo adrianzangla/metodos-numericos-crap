7fec8d1c08301856523c0d9f7e21248c17de648f
adrianzangla <adrianzangla@outlook.com>
2023-06-25 04:44:27 -0300

    improved gauss seidel and inverse power methods. inverse power method now uses gauss_seidel when applicable

--- gauss_seidel.m
+++ gauss_seidel.m
@@ -1,41 +1,11 @@
-function xk1 = gauss_seidel(A, b, xk, ef, ea, er, maxIter)
-
-  T = getT(A);
-  c = getc(A,b);
-
-  k = 0;
-  nhs = true;
-
-  while nhs
-
-    tabla(k+1,1) = k;
-    for i=1:rows(xk)
-      tabla(k+1,i+1) = xk(i);
-    endfor
-
-    k += 1;
-
-    for i=1:rows(T)
-      xk1(i,1) = 0;
-      for j=1:i-1
-        xk1(i,1) += T(i,j)*xk1(j,1);
-      endfor
-      for j=i+1:columns(T)
-        xk1(i,1) += T(i,j)*xk(j);
-      endfor
-      xk1(i,1) += c(i);
-    endfor
-
-    nhs = max(A*xk1!=b) ...
-    && norm(A*xk1 - b,Inf) > ef ...
-    && norm(xk1-xk,Inf) > ea ...
-    && norm(xk1-xk,Inf)/norm(xk1,Inf) > er ...
-    && k < maxIter;
-
-    xk = xk1;
-
-  endwhile
-
-  display(tabla);
-
+function x = gauss_seidel(A, b, x, e, m)
+  for k=1:m
+    for i=1:length(x)
+      x(i) = (b(i) - A(i,1:i-1)*x(1:i-1) - A(i,i+1:length(x))*x(i+1:length(x)))/A(i,i);
+    end
+    if norm(A*x-b) < e
+      display(k);
+      break;
+    end
+  end
 endfunction
--- potencia_inversa.m
+++ potencia_inversa.m
@@ -1,13 +1,17 @@
 function [v, l] = potencia_inversa(A, w, e, m)
-    w /= norm(w);
-    for i = 1:m
-        v = A\w;
-        v /= norm(v);
-        if abs(v' * w) > 1-e
-            display(i);
-            break;
-        end
-        w = v;
-    end
-    l = 1/(v'*(A\v));
+  w /= norm(w);
+  for k = 1:m
+    if all(diag(A))
+      v = gauss_seidel(A, w, ones(length(w),1), e, m);
+    else
+      v = A\w;
+    endif
+    v /= norm(v);
+    if abs(v' * w) > 1-e
+      display(k);
+      break;
+    endif
+    w = v;
+  endfor
+  l = 1/(v'*(A\v));
 end
